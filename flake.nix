83c1a1be065d959cb19180c9299ea305ff9fcd6f
Richard Warfield <richard@litx.io>
2024-01-27 15:55:20 +0700

    Change to submodules

--- flake.nix
+++ flake.nix
@@ -1,13 +1,11 @@
 {
   
-  inputs.proj1.url = "git+ssh://git@github.com/RichardWarfield/proj1.git";
-
   outputs = { self, nixpkgs, proj1 }: {
     defaultPackage.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
       name = "hello-world-lib";
       src = ./.;
       buildPhase = ''
-        cc -o hello_world ${proj1.defaultPackage.x86_64-linux}/hello_world.o
+        cc -o hello_world ./proj1/src/hello_world.c
       '';
 
       installPhase = ''
