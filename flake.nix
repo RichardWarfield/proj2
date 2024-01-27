568d74cca813481b3fc42079600dd94d4305b63e
Richard Warfield <richard@litx.io>
2024-01-27 15:56:34 +0700

    Hi

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   
-  outputs = { self, nixpkgs, proj1 }: {
+  outputs = { self, nixpkgs }: {
     defaultPackage.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
       name = "hello-world-lib";
       src = ./.;
